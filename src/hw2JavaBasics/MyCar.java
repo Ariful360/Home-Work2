6aa0befcea7ff623c9e084ae8a34f0d1314106ca
ariful <arfulislmqa@gmail.com>
2022-10-25 22:10:02 -0400

    home work

---
+++ src/hw2JavaBasics/MyCar.java
@@ -0,0 +1,17 @@
+package hw2JavaBasics;
+
+import javax.naming.InitialContext;
+
+public class MyCar {
+	public static void main(String[] args) {
+		String name = "Ariful";
+		 
+		System.out.println("My name is \"Ariful\"," + "\nI love to dive MyCar car");
+		
+		System.out.print("model: Tlx");
+		System.out.print(" Color: White");
+		System.out.print(" Year: 2015");
+		System.out.print(" Wheel Drive: FWD");
+	}
+
+}
