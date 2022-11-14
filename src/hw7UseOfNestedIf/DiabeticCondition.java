c829f8de07fee2142b3c4a084f011997814237f9
ariful <arfulislmqa@gmail.com>
2022-11-14 03:59:49 -0500

    hw07

--- src/hw7UseOfNestedIf/DiabeticCondition.java
+++ src/hw7UseOfNestedIf/DiabeticCondition.java
@@ -9,12 +9,12 @@ public class DiabeticCondition {
 		Scanner scanner = new Scanner(System.in);
 		double hbga1c = scanner.nextDouble();
 		
-		if (hbga1c > 6.4) {
+		if (hbga1c > 6.4) { // Outer if block
 			System.out.println("I am a diabetic patient");
 			
 		} else {
 			
-			if (hbga1c >= 5.7) {
+			if (hbga1c >= 5.7) { // Inner if or nested if block
 				System.out.println("I am a pre-diabetic patient");
 				
 			}else if (hbga1c < 5.7) {
